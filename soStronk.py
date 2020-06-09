e586fccd75a2451f9f9bc68852ad760e52494542
4d4rshkumar <44334235+4d4rshkumar@users.noreply.github.com>
2020-06-09 11:27:03 +0530

    Add files via upload
    fixed bug, showing wrong behaviour on players having same scores.

--- soStronk.py
+++ soStronk.py
@@ -8,6 +8,7 @@
     if this happens match-making is not possible
 5. Pass the players, array of name and score and length of array in subset function
     this function chooses the unique subsets if length given in players
+
     this function works as converting numbers from 1- (2 raise to power length of array )-1 to binary
     then wherever it finds sum of 1's in binary equals to players in team it appends to a binop list
 6. A new dictionary is created and name of persons in team and their average score is stored, score is the
@@ -16,6 +17,7 @@
 8.  All the keys are taken and sorted
 9. From sorted keys middle 2 keys are taken and shown output and then removed from the list
 10. this process is repeated till the list is empty
+
 '''
 def subset(m,lst,l):
     '''------------------------finding the subsets---------------------------'''
@@ -41,7 +43,7 @@ def subset(m,lst,l):
                 name += lst[acc][0] + ','
                 score += round(int(lst[acc][1]) / m, 2)  # computing average
             acc += 1
-        dic[score] = name[:-1]
+        dic[name[:-1]] = score
     return dic
 
 
@@ -63,19 +65,47 @@ length = len(arr)
 
 '''------------------------if m equals no of players-------------------'''
 
-if (length <= players or (players==2 and length==3) or length==3 ):
+if (length <= players or (players==2 and length==3) or length==3 and length%2==0):
     print("Match is not possible")
     exit()
 required_data=subset(players,arr,length)
 '''--------------------printing the quality of matches--------------------'''
 avg_scores=[]
 for i in required_data:
-    avg_scores.append(i)
+    avg_scores.append(required_data[i])
 avg_scores.sort()
+
+
+
+def get_key(val):
+    for key, value in required_data.items():
+         if val == value:
+             return key
+
+
 while len(avg_scores) != 0:
     tmp = (len(avg_scores) / 2) - 1
     a = avg_scores[int(tmp)]
     b = avg_scores[int(tmp + 1)]
     avg_scores.remove(a)
     avg_scores.remove(b)
-    print(required_data[a], "(", a, ") vs ", required_data[b], '(', b, ')')
+    try:
+        print(get_key(a), "(", a, ") vs ",end='')
+        check=get_key(a).split(',')
+        required_data.pop(get_key(a))
+        name=''
+        score=''
+        removed={}
+        '''---------------------to check same player does not encounter himself in opposition------------------'''
+        for i in check:
+            to_ck=get_key(b)
+            if i in to_ck:
+                removed[to_ck]=b
+                required_data.pop(get_key(b))
+
+        print(get_key(b), '(', b, ')')
+        required_data.pop(get_key(b))
+        for i in removed:
+            required_data[i]=removed[i]
+    except:
+        print("MATCH NOT POSSIBLE!")
--- test_soStronk.py
+++ test_soStronk.py
@@ -3,28 +3,18 @@ import soStronk
 '''=======================================test cases==================================================================='''
 class test_calc(unittest.TestCase):
     def test_case0(self):
-        result = soStronk.subset(1,[['bleh', '85'], ['Aequitas', '90'], ['akS', '87'], ['lamiV', '20']],4)
-        self.assertEqual(result,{20.0: 'lamiV', 87.0: 'akS', 90.0: 'Aequitas', 85.0: 'bleh'})
+        result = soStronk.subset(2,[['bleh', '85'], ['aequitas', '90'], ['aks', '87'], ['lamiV', '20']],4)
+        self.assertEqual(result,{'aks,lamiV': 53.5, 'aequitas,lamiV': 55.0, 'aequitas,aks': 88.5, 'bleh,lamiV': 52.5, 'bleh,aks': 86.0, 'bleh,aequitas': 87.5})
 
 
     def test_case1(self):
-        result = soStronk.subset(2,[['bleh', '85'], ['Aequitas', '90'], ['akS', '87'], ['lamiV', '20']],4)
-        self.assertEqual(result,{53.5: 'akS,lamiV', 55.0: 'Aequitas,lamiV', 88.5: 'Aequitas,akS', 52.5: 'bleh,lamiV', 86.0: 'bleh,akS', 87.5: 'bleh,Aequitas'})
+        result = soStronk.subset(3,[['goal', '21'], ['lol', '345'], ['soo', '987'], ['good', '345'], ['knight', '45'], ['hunter', '678']],6)
+        self.assertEqual(result,{'good,knight,hunter': 356.0, 'soo,knight,hunter': 570.0, 'soo,good,hunter': 670.0, 'soo,good,knight': 459.0, 'lol,knight,hunter': 356.0, 'lol,good,hunter': 456.0, 'lol,good,knight': 245.0, 'lol,soo,hunter': 670.0, 'lol,soo,knight': 459.0, 'lol,soo,good': 559.0, 'goal,knight,hunter': 248.0, 'goal,good,hunter': 348.0, 'goal,good,knight': 137.0, 'goal,soo,hunter': 562.0, 'goal,soo,knight': 351.0, 'goal,soo,good': 451.0, 'goal,lol,hunter': 348.0, 'goal,lol,knight': 137.0, 'goal,lol,good': 237.0, 'goal,lol,soo': 451.0})
 
 
     def test_case2(self):
-        result = soStronk.subset(3,[['bleh', '85'], ['Aequitas', '90'], ['akS', '87'], ['lamiV', '20']],4)
-        self.assertEqual(result,{65.67: 'Aequitas,akS,lamiV', 64.0: 'bleh,akS,lamiV', 65.0: 'bleh,Aequitas,lamiV', 87.33: 'bleh,Aequitas,akS'})
-
-
-    def test_case3(self):
-        result = soStronk.subset(2,[['a', '34'], ['b', '78'], ['c', '98'], ['d', '09'], ['e', '45']],5)
-        self.assertEqual(result,{27.0: 'd,e', 71.5: 'c,e', 53.5: 'c,d', 61.5: 'b,e', 43.5: 'b,d', 88.0: 'b,c', 39.5: 'a,e', 21.5: 'a,d', 66.0: 'a,c', 56.0: 'a,b'})
-
-
-    def test_case4(self):
-        result = soStronk.subset(3,[['a', '54'], ['b', '87'], ['c', '67'], ['d', '99'], ['e', '55'], ['f', '89'], ['g', '78']],7)
-        self.assertEqual(result,{74.0: 'e,f,g', 88.67: 'd,f,g', 77.33: 'b,c,g', 81.0: 'b,c,f', 78.0: 'c,f,g', 66.66: 'c,e,g', 70.33: 'c,e,f', 81.33: 'c,d,g', 85.0: 'c,d,f', 73.66: 'c,d,e', 84.67: 'b,f,g', 73.33: 'a,c,d', 77.0: 'a,d,g', 88.0: 'b,d,g', 91.67: 'b,d,f', 80.33: 'b,d,e', 69.66: 'b,c,e', 84.33: 'b,c,d', 73.67: 'a,f,g', 62.33: 'a,e,g', 66.0: 'a,e,f', 80.67: 'a,d,f', 69.33: 'a,b,c', 66.33: 'a,c,g', 70.0: 'a,c,f', 58.66: 'a,c,e', 73.0: 'a,b,g', 76.67: 'a,b,f', 65.33: 'a,b,e', 80.0: 'a,b,d'})
+        result = soStronk.subset(1,[['asd', '123'], ['bff', '234'], ['cnd', '345'], ['gost', '456']],4)
+        self.assertEqual(result,{'gost': 456.0, 'cnd': 345.0, 'bff': 234.0, 'asd': 123.0})
 
 
 if __name__ =="__main__":
